943f415fea47d1fdce9dad9d7d42e26a76b804e9
yash968 <yashmathurbpt@gmail.com>
2023-01-08 11:50:34 +0530

    first commit

---
+++ src/Component/MenuApi.js
@@ -0,0 +1,88 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/download.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Made with a blend of finely ground and whole spices, and herbs, the all new Maggi instant noodles have an extra special and flavourful tastemaker sachet inside. These spices have been roasted to perfection, providing a distinct aroma and colour to your Maggi noodles. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/naan.jpg",
+      name: "naan",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Naan is a white leavened flatbread that is traditionally cooked in a tandoor. By baking the naan in a tandoor, the distinct charred sections and crispy edges are created; Two things that make naan such a well-known and loved addition to Indian cuisine. ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: " Sweet corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Among all the types of corn, sweet corn is rather unique: Whether yellow, white, or bicolored, all varieties of sweet corn are the result of natural mutations of corn genes that code for starch; these mutations cause sweet corn to have a sugar content that is twice that of field corn and less starch.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chole Bhature",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "It is a combination of chana masala (spicy white chickpeas) and bhatura/puri, a deep-fried bread made from maida. Although it is known as a typical Punjabi dish, It is originally a culinary dish of Delhi and Uttar Pradesh. ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "A delicious pizza has an intensely cheesy flavor. The combination of tomato sauce and cheese creates a perfect marriage of flavor. The ingredients begin to brown during the baking process, which makes pizza taste so delicious ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A typical non-veg meal includes meat or fish in it along with other vegetarian dishes - for example, dal or sabji (dry curry). My non-vegetarian thali/meals in this collection include not just delicious meat dishes, but also some mouthwatering vegetarian dishes ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "des·​sert di-ˈzərt. : a usually sweet course or dish (as of pastry or ice cream) usually served at the end of a meal. British : a fresh fruit served after a sweet course ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "rajma chawal  tastes lipsmacking when served hot with steamed rice and now it has turned out to be one of the most favorite comfort foods at our home too. We know Rajma is rich in iron and protein and when it combines with rice, it makes a great source of carbohydrates. ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "Samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "A samosa (/səˈmoʊsə/) or singara is a fried South Asian pastry with a savory filling, including ingredients such as spiced potatoes, onions, and peas. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region. ",
+    },
+    
+  ];
+  
+  export default Menu;
+  
